77fed300b291371ebd7fed963c4a71f1e1c096f3
Martin Czygan <martin.czygan@gmail.com>
2023-03-10 17:42:49 +0100

    vault: update docs

--- backend/vault/oapi/doc.go
+++ backend/vault/oapi/doc.go
@@ -1,2 +1,5 @@
+// Package oapi wrap the vault API. The openapi schema.json file should matcht
+// that from vault-site repo.
+//
 //go:generate oapi-codegen -generate types,client,spec -package oapi -o vault.gen.go schema.json
 package oapi
