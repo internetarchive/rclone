ce28aefab97beb8610f30912884c990e08a4e57e
Martin Czygan <martin.czygan@gmail.com>
2023-04-19 23:32:43 +0200

    vault: init needs to be root

--- backend/vault/vault.go
+++ backend/vault/vault.go
@@ -8,6 +8,88 @@ import (
 	"github.com/rclone/rclone/fs/config/configmap"
 )
 
+func init() {
+	fs.Register(&fs.RegInfo{
+		Name:        "vault",
+		Description: "Internet Archive Vault Digital Preservation System",
+		NewFs:       NewFs,
+		Options: []fs.Option{
+			{
+				Name:    "username",
+				Help:    "Vault username",
+				Default: "",
+			},
+			{
+				Name:    "password",
+				Help:    "Vault password",
+				Default: "",
+			},
+			{
+				Name:    "endpoint",
+				Help:    "Vault API endpoint URL",
+				Default: "http://127.0.0.1:8000/api",
+			},
+			{
+				Name:    "suppress_progress_bar",
+				Help:    "Suppress deposit progress bar",
+				Default: false,
+				Hide:    fs.OptionHideConfigurator,
+			},
+			{
+				Name:    "skip_content_type_detection",
+				Help:    "Skip content-type detection on the client side",
+				Default: false,
+				Hide:    fs.OptionHideConfigurator,
+			},
+			{
+				Name:    "resume_deposit_id",
+				Help:    "Resume a deposit",
+				Default: 0,
+				Hide:    fs.OptionHideConfigurator,
+			},
+			{
+				Name:     "chunk_size",
+				Help:     "Upload chunk size in bytes (limited)",
+				Default:  defaultUploadChunkSize,
+				Advanced: true,
+			},
+			{
+				Name:     "max_parallel_chunks",
+				Help:     "Maximum number of parallel chunk uploads",
+				Default:  defaultMaxParallelChunks, // TODO: find a good default
+				Advanced: true,
+			},
+			{
+				Name:     "max_parallel_uploads",
+				Help:     "Maximum number of parallel file uploads",
+				Default:  defaultMaxParallelUploads, // TODO: find a good default
+				Advanced: true,
+			},
+		},
+		CommandHelp: []fs.CommandHelp{
+			fs.CommandHelp{
+				Name:  "status",
+				Short: "show deposit status",
+				Long: `Display status of deposit, pass deposit id (e.g. 742) as argument, e.g.:
+
+    $ rclone backend ds vault: 742
+
+Will return a JSON like this:
+
+    {
+      "assembled_files": 6,
+      "errored_files": 0,
+      "file_queue": 0,
+      "in_storage_files": 0,
+      "total_files": 6,
+      "uploaded_files": 0
+    }
+`,
+			},
+		},
+	})
+}
+
 // NewFS sets up a new filesystem for vault.
 func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, error) {
 	return v2.NewFs(ctx, name, root, m)
