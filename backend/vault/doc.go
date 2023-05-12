f08a6144eb4331f49cfd1a399d97bb95d26a1597
Martin Czygan <martin.czygan@gmail.com>
2023-05-12 16:42:16 +0200

    vault: update docs

--- backend/vault/doc.go
+++ backend/vault/doc.go
@@ -1,3 +1,4 @@
-// Package vault adds support for the Vault Digital Preservation System at
-// Internet Archive.
+// Package vault adds support for the Vault Digital Preservation System
+// developed and hosted at the Internet Archive. Find out more at:
+// https://support.archive-it.org/hc/en-us/sections/7581093252628-Vault.
 package vault
