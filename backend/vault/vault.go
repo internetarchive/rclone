fa2a7ecd3a5385f53ff4df4ecba6d98761a2d3c7
Martin Czygan <martin.czygan@gmail.com>
2023-06-01 10:40:53 +0200

    vault: note on chunk size issue

--- backend/vault/vault.go
+++ backend/vault/vault.go
@@ -18,6 +18,33 @@ const (
 	//
 	// Target two-core QA machine was occassionally maxed out, not sure if
 	// that's imposing a limit.
+	//
+	// ----8<----
+	//
+	// As of 05/2023, we reduced the default chunk size to 1M, temporarily
+	// after encountering some issues.
+	//
+	// ----8<----
+	//
+	// @martin I'm just remembering to ask about this, but could you fill me in
+	// on the rationale for decreasing the chunk sizes that rclone sends to
+	// vault?
+	//
+	// yes, happily; in short: I was getting a HTTP 404 from chunk upload and I
+	// believe it came from this line:
+	// vault-site:19e168e41489e2278bb59c659489344857a04f4e/vault/deposit_api.py#L193-199
+	// (not too many 404's possible otherwise) -- now the deposit is registered
+	// successfully, so the deposit's state must have changed (to something
+	// other than "REGISTERED") -- and that (only) happens, when a deposit is
+	// "completed" -- which happens e.g. here:
+	// vault-site:19e168e41489e2278bb59c659489344857a04f4e/vault/asynchronous/temporal/deposit/workflow.py#L62-66
+	// -- so my wild guess ATM is that we encounter some break here:
+	// vault-site:19e168e41489e2278bb59c659489344857a04f4e/vault/asynchronous/temporal/deposit/workflow.py#L24-60
+	// -- and that causes the deposit to be "completed" early, which then causes
+	// a 404 when trying to upload a chunk somehow, lowering the chunksize made
+	// this problem go away, but there's probably a better way to handle this --
+	// would be glad to have a short in person debug session (where we can try
+	// to replicate the issue in prod together, or the like)
 	defaultUploadChunkSize = 1 << 20 // 1M
 )
 
